3c2150385c7a0a9acd6db3bd16d5523990194780
Henry Kemboi <lilanhenry@gmail.com>
2024-01-05 14:47:36 +0300

    final commit

---
+++ odd_even.py
@@ -0,0 +1,9 @@
+# Number to check?
+number = int(input(print("Which number do you want to check?")))
+
+# Write your code below
+
+if number % 2 == 0:
+  print("This is an even number")
+else:
+  print("This is an odd number")
